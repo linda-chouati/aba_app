33bfe2a433fe97d811723181acf827b013cb6d5c
Chouati Linda <linda.chouati@gmail.com>
2025-09-25 12:47:35 +0200

    ...

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], allow_credentials=True,
     allow_methods=["*"], allow_headers=["*"],
+    allow_credentials=False,
 )
 
 @app.get("/health")
